ab8482348b72140eadab541ea7222400e65eeb24
foxzin-0635 <jaoviniciussantiago988@gmail.com>
2026-06-22 20:46:18 -0300

    Update Object.lua

--- src/classes/Object.lua
+++ src/classes/Object.lua
@@ -1,4 +1,18 @@
-local Object_metadata = {
+local Object_metadata = {}
+
+local function isA(self, className: string): boolean
+  local it = Object_metadata.inheritTree
+  local bt = string.split(it, ",")
+  
+  local v = false
+  for i = 1, #bt do
+    if className == bt[i] then v = true break end
+  end
+  
+  return v
+end
+
+Object_metadata = {
   inheritTree = "Object", -- for :IsA(className)
   members = {
     ClassName = {
@@ -19,40 +33,28 @@ local Object_metadata = {
   
 }
 
-local function isA(self, className: string): boolean
-  local it = self.inheritTree
-  local bt = string.split(it, ",")
-  
-  local v = false
-  for i = 1, #bt do
-    if className == bt[i] then v = true break end
-  end
-  
-  return v
-end
-
 local Object = {}
 
 setmetatable(Object, {
   __metatable = "The metatable is locked.",
   __index = function(t,k)
-    for mk,t in pairs(Object_metadata.members) do
+    for mk,mt in pairs(Object_metadata.members) do
       if mk == k then
-        return t.Value
+        return mt.Value
       end
     end
   end,
   __tostring = Object.ClassName,
   __newindex = function(t, k, v)
-    for mk,t in pairs(Object_metadata.members) do
+    for mk,mt in pairs(Object_metadata.members) do
       if mk == k then
-        if t.ReadOnly then
+        if mt.ReadOnly then
           error("Unable to assign property "..k..". Property is read only")
         else
-          if type(v) == t.ValueType then
-            t.Value = v
+          if type(v) == mt.ValueType then
+            mt.Value = v
           else
-            error("Type '"..type(v).."' could not be converted into '"..t.ValueType.."'")
+            error("Type '"..type(v).."' could not be converted into '"..mt.ValueType.."'")
           end
         end
       end
